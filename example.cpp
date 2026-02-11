889c8165ed499208aa3c80f5d3ce4aa27c973dd7
Ukulele-beep <prime.sokolov.27@gmail.com>
2026-02-11 13:01:26 +0300

    example.cpp

--- example.cpp
+++ example.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+void greet(const std::string& name) {
+    std::cout << "Hello, " << name << "!" << std::endl;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    greet("GitHub");
     return 0;
 }
